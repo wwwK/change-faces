0d7737d8d2c091a0884a9b5c1c046e168bf7267d
Devin Chen <d3vin.chen@gmail.com>
2017-08-12 15:51:14 +0800

    add  fabric

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <p>{{ foo }}</p>
+    <canvas id="canvas3" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-console */
+
 export default {
   name: 'app',
+  data() {
+    return {
+      foo: 'bar',
+    };
+  },
+  mounted() {
+    try {
+      // create a wrapper around native canvas element (with id="c")
+      const canvas = new fabric.Canvas('canvas3');
+
+      // create a rectangle object
+      const rect = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20,
+      });
+
+      // "add" rectangle onto canvas
+      canvas.add(rect);
+    } catch (error) {
+      console.log('something is wrong with fabric init:', error);
+    }
+  },
 };
 </script>
 
